874e1fb5da9cba50fa7d71b7b2365547411fe1e5
Marco Medrano <marco.medrano.garay@gmail.com>
2024-01-18 02:11:55 -0400

    feat: making it work as static

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,8 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Radzen;
-
 using PolyWeb.Client;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -11,6 +10,9 @@ builder.Services.AddScoped<NotificationService>();
 builder.Services.AddScoped<TooltipService>();
 builder.Services.AddScoped<ContextMenuService>();
 builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var host = builder.Build();
 await host.RunAsync();
--- Client/Shared/MainLayout.razor.cs
+++ Client/Shared/MainLayout.razor.cs
@@ -1,14 +1,6 @@
-using System.Net.Http;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Components.Routing;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.Web.Virtualization;
 using Microsoft.JSInterop;
 using Radzen;
-using Radzen.Blazor;
 
 namespace PolyWeb.Client.Shared
 {
@@ -37,6 +29,11 @@ namespace PolyWeb.Client.Shared
         void SidebarToggleClick()
         {
             sidebarExpanded = !sidebarExpanded;
+        }
+
+        protected override void OnInitialized()
+        {
+            sidebarExpanded = false;
         }
     }
 }
