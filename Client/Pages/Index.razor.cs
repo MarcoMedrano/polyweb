c4f7c66545f3be23c1bd787d17f0e99c834cfa66
Marco Medrano <marco.medrano.garay@gmail.com>
2024-02-07 20:05:56 -0400

    feat: x64 x86 buttons

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -1,35 +1,29 @@
-using System.Net.Http;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Components.Routing;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.Web.Virtualization;
 using Microsoft.JSInterop;
 using Radzen;
-using Radzen.Blazor;
 
-namespace PolyWeb.Client.Pages
+namespace PolyWeb.Client.Pages;
+
+public partial class Index
 {
-    public partial class Index
-    {
-        [Inject]
-        protected IJSRuntime JSRuntime { get; set; }
+    [Inject]
+    protected IJSRuntime JSRuntime { get; set; }
 
-        [Inject]
-        protected NavigationManager NavigationManager { get; set; }
+    [Inject]
+    protected NavigationManager NavigationManager { get; set; }
 
-        [Inject]
-        protected DialogService DialogService { get; set; }
+    [Inject]
+    protected DialogService DialogService { get; set; }
 
-        [Inject]
-        protected TooltipService TooltipService { get; set; }
+    [Inject]
+    protected TooltipService TooltipService { get; set; }
 
-        [Inject]
-        protected ContextMenuService ContextMenuService { get; set; }
+    [Inject]
+    protected ContextMenuService ContextMenuService { get; set; }
 
-        [Inject]
-        protected NotificationService NotificationService { get; set; }
-    }
+    [Inject]
+    protected NotificationService NotificationService { get; set; }
+
+    private void DownloadWindowsx86() => NavigationManager.NavigateTo("https://polytranslator.s3.amazonaws.com/windows/x86/0.0.0.0/Poly.msi", forceLoad: true);
+    private void DownloadWindowsx64() => NavigationManager.NavigateTo("https://polytranslator.s3.amazonaws.com/windows/x64/0.0.0.0/Poly.msi", forceLoad: true);
 }
